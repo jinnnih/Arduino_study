0980f84f39d089813e9c76ce4f4cf8fe79d2fa9a
hyunjin <jinnnih@gmail.com>
2026-02-10 16:28:02 +0900

    특정거리 이하일때 바퀴 멈추기

---
+++ motorDriver2/motorDriver2.ino
@@ -0,0 +1,45 @@
+const int trigPin = 9;
+const int echoPin = 10;
+int AA = 6;
+int AB = 7;
+
+
+long duration;
+int distance;
+void setup() {
+  pinMode(AA, OUTPUT);
+  pinMode(AB, OUTPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+
+  if (distance >= 20) {
+    digitalWrite(AA, HIGH);
+    digitalWrite(AB, LOW);
+    
+  } else {
+    digitalWrite(AA, LOW);
+    digitalWrite(AB, LOW);
+    
+  }
+
+  if (distance == 0) return;
+  delay(500);
+}
